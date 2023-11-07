55a4e281f3cc2eb415919a7c943f04ca356dfe99
John Lian <jlian@users.noreply.github.com>
2023-11-07 18:46:47 +0000

    Fix k9s install

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -18,7 +18,7 @@ export K3D_FIX_MOUNTS=1
 
 # Create k3d cluster with NFS support and forwarded ports
 # See https://github.com/jlian/k3d-nfs
-k3d cluster create $CLUSTER_NAME -i ghcr.io/jlian/k3d-nfs:v1.25.3-k3s1 \
+k3d cluster create -i ghcr.io/jlian/k3d-nfs:v1.25.3-k3s1 \
 -p '1883:1883@loadbalancer' \
 -p '8883:8883@loadbalancer' \
 -p '6001:6001@loadbalancer' \
