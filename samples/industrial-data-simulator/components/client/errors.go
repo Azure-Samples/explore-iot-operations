9287f54122e4e7234ada85467845c3f4b1472cec
Ethan <ethanperry@microsoft.com>
2023-11-15 11:24:28 -0800

    Renaming for device simulator

---
+++ samples/industrial-data-simulator/components/client/errors.go
@@ -0,0 +1,35 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+package client
+
+import (
+	"fmt"
+
+	"github.com/explore-iot-ops/samples/industrial-data-simulator/lib/errors"
+)
+
+type BrokerConnectionError struct {
+	errors.BadRequest
+	id       string
+	endpoint string
+	err      error
+}
+
+func (err *BrokerConnectionError) Error() string {
+	return fmt.Sprintf(
+		"mqtt client with id=%s could not connect to MQTT broker at endpoint %s: %q",
+		err.id,
+		err.endpoint,
+		err.err.Error(),
+	)
+}
+
+type UnknownClientTypeError struct {
+	errors.BadRequest
+	name string
+}
+
+func (err *UnknownClientTypeError) Error() string {
+	return fmt.Sprintf("no such %s type for mqtt client component", err.name)
+}
---
+++ samples/industrial-data-simulator/components/renderer/service_test.go
@@ -0,0 +1,90 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+package renderer
+
+import (
+	"testing"
+
+	"github.com/explore-iot-ops/samples/industrial-data-simulator/components/formatter"
+	"github.com/explore-iot-ops/samples/industrial-data-simulator/lib/component"
+	"github.com/explore-iot-ops/samples/industrial-data-simulator/lib/composition"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	MockID          = "MockID"
+	MockFormatterID = "MockFormatterID"
+	MockNodeID      = "MockNodeID"
+)
+
+func TestStore(t *testing.T) {
+	store := NewStore()
+	_, ok := store.(*component.Memstore[Renderer, component.ID])
+	require.True(t, ok)
+}
+
+func TestRendererService(t *testing.T) {
+	service := NewService(&component.MockStore[Renderer, component.ID]{
+		OnCreate: func(entity Renderer, identifier component.ID) error {
+			require.Equal(t, MockID, string(identifier))
+			return nil
+		},
+	}, &component.MockStore[formatter.Formatter, component.ID]{
+		OnGet: func(identifier component.ID) (formatter.Formatter, error) {
+			require.Equal(t, MockFormatterID, string(identifier))
+			return nil, nil
+		},
+	}, &component.MockStore[composition.Renderer, component.ID]{
+		OnGet: func(identifier component.ID) (composition.Renderer, error) {
+			require.Equal(t, MockNodeID, string(identifier))
+			return nil, nil
+		},
+	})
+
+	err := service.Create(MockID, &Component{
+		FormatterID: MockFormatterID,
+		NodeID:      MockNodeID,
+	})
+	require.NoError(t, err)
+}
+
+func TestRendererServiceNodeStoreError(t *testing.T) {
+	service := NewService(
+		nil,
+		&component.MockStore[formatter.Formatter, component.ID]{
+			OnGet: func(identifier component.ID) (formatter.Formatter, error) {
+				return nil, nil
+			},
+		},
+		&component.MockStore[composition.Renderer, component.ID]{
+			OnGet: func(identifier component.ID) (composition.Renderer, error) {
+				return nil, &component.MockError{}
+			},
+		},
+	)
+
+	err := service.Create(MockID, &Component{
+		FormatterID: MockFormatterID,
+		NodeID:      MockNodeID,
+	})
+	require.Equal(t, &component.MockError{}, err)
+}
+
+func TestRendererServiceFormatterStoreError(t *testing.T) {
+	service := NewService(
+		nil,
+		&component.MockStore[formatter.Formatter, component.ID]{
+			OnGet: func(identifier component.ID) (formatter.Formatter, error) {
+				return nil, &component.MockError{}
+			},
+		},
+		nil,
+	)
+
+	err := service.Create(MockID, &Component{
+		FormatterID: MockFormatterID,
+		NodeID:      MockNodeID,
+	})
+	require.Equal(t, &component.MockError{}, err)
+}
