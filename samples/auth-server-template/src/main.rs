a34b6e22564f83abe3a0fe4fab20f12b63d01392
John Lian <jlian@users.noreply.github.com>
2023-11-15 09:08:30 -0800

    Improve copyright notice

--- samples/auth-server-template/src/main.rs
+++ samples/auth-server-template/src/main.rs
@@ -1,4 +1,5 @@
-// Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 
 /// Handles authentication of MQTT connections.
 mod authenticate;
