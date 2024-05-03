73b5c7c414274faeb4d6fc7384060d1940ed08ff
John Lian <jlian@users.noreply.github.com>
2024-05-03 18:08:23 +0000

    Remove unused lines

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -9,19 +9,10 @@ echo "Starting On Create Command"
 # Copy the custom first run notice over
 sudo cp .devcontainer/welcome.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
 
-# This env var is important to allow k3s to support shared mounts, required for CSI driver
-# Temporary fix until made default https://github.com/k3d-io/k3d/pull/1268#issuecomment-1745466499
-export K3D_FIX_MOUNTS=1
-
-# Set the cluster name to the name of the cluster created by the GitHub Codespace
-export CLUSTER_NAME=$(gh cs view --json name --jq '.name')
-
 # Create k3d cluster and forwarded ports
 k3d cluster delete
 k3d cluster create \
 -p '1883:1883@loadbalancer' \
--p '8883:8883@loadbalancer' \
--p '6001:6001@loadbalancer' \
--p '4000:80@loadbalancer'
+-p '8883:8883@loadbalancer'
 
 echo "Ending On Create Command"
