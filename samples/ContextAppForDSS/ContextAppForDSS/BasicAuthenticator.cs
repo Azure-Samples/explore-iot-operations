add0bb420606fbb6420b1250bbe3adceab33b86d
Oliva Kar <olkar@microsoft.com>
2024-08-13 15:36:04 -0700

    first set

---
+++ samples/ContextAppForDSS/ContextAppForDSS/BasicAuthenticator.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace ContextualDataIngestor
+{
+    public class BasicAuthenticator : IAuthenticator
+    {
+        private readonly string _username;
+        private readonly string _password;
+
+        public BasicAuthenticator(string username, string password)
+        {
+            _username = username ?? throw new ArgumentNullException(nameof(username));
+            _password = password ?? throw new ArgumentNullException(nameof(password));
+        }
+
+        public void ApplyAuthentication(HttpClient httpClient)
+        {
+            var byteArray = Encoding.ASCII.GetBytes($"{_username}:{_password}");
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+        }
+    }
+}
---
+++ samples/ContextAppForDSS/ContextAppForDSS/UserConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace ContextualDataIngestor
+{
+    internal class UserConfig
+    {
+        public required string EndpointType { get; set; }
+        public required string ConnectionStringOrBaseUrl { get; set; }
+        public required string Endpoint { get; set; }
+        public string? RequestBody { get; set; }
+        public string? DataFormat { get; set; }
+        public string? QueryParams { get; set; }
+    }
+}
