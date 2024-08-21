440e02e8ca19a934a04ba50e71d59f9137222acf
Oliva Kar <olkar@microsoft.com>
2024-08-21 14:35:13 -0700

    remove comments

--- samples/context-app-dotnet/ContextAppForDSS/SqlDataRetriever .cs
+++ samples/context-app-dotnet/ContextAppForDSS/SqlDataRetriever .cs
@@ -16,16 +16,12 @@ namespace ContextualDataIngestor
         private string _connectionString;
         private string _query;
         private readonly IAuthStrategy _authConfig;
-        //ILogger logger;
 
         public SqlDataRetriever(SqlRetrievalConfig retrievalConfig, IAuthStrategy authConfig)
         {
             _connectionString = $"Server={retrievalConfig.ServerName};Database={retrievalConfig.DatabaseName};";
             _query = $"SELECT * FROM {retrievalConfig.TableName}";
             _authConfig = authConfig;
-            //using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
-            //ILogger logger = factory.CreateLogger("SqlDataRetriever");
-
         }
 
         private void Authenticate()
@@ -40,26 +36,19 @@ namespace ContextualDataIngestor
         public async Task<string> RetrieveDataAsync()
         {
             Authenticate();
-            // StringBuilder to accumulate the formatted strings
             StringBuilder result = new StringBuilder();
 
-            // Create a SQL connection object
             using (SqlConnection connection = new SqlConnection(_connectionString))
             {
                 try
                 {
-                    // Open the connection to the database asynchronously
                     await connection.OpenAsync();
-                    // Create a SQL command object
                     using (SqlCommand command = new SqlCommand(_query, connection))
                     {
-                        // Execute the query and retrieve the data using a data reader asynchronously
                         using (SqlDataReader reader = await command.ExecuteReaderAsync())
                         {
-                            // Check if there are rows to read
                             if (reader.HasRows)
                             {
-                                // Read each row and format the data as a JSON-like string
                                 while (await reader.ReadAsync())
                                 {
                                     string formattedRow = $"{{ \"country\" : \"{reader["Country"]}\" , \"viscosity\" : {reader["Viscosity"]}, \"sweetness\" : {reader["Sweetness"]}, \"particle_size\" : {reader["ParticleSize"]}, \"overall\" : {reader["Overall"]} }}";
@@ -79,7 +68,6 @@ namespace ContextualDataIngestor
                 }
             }
 
-            // Output the formatted result
             return result.ToString();
         }
 
