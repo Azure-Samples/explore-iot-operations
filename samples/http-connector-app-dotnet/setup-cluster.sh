ed8bebaeb578749d33c947ee8af07652eb763b38
Tim Taylor <timtay@microsoft.com>
2024-10-01 21:46:38 +0000

    asdf

--- samples/http-connector-app-dotnet/setup-cluster.sh
+++ samples/http-connector-app-dotnet/setup-cluster.sh
@@ -1,17 +1,19 @@
-sudo apt-get update
-
-# Install k3d
-wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
-
-# Install helm
-curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-
-# Install .NET 8.0
-sudo apt-get install -y dotnet-sdk-8.0
-
 # Create k3d cluster with local image registry
+k3d registry delete registry.localhost
+k3d cluster delete
+k3d registry create registry.localhost --port 5500
+k3d cluster create -p '1883:1883@loadbalancer' -p '8883:8883@loadbalancer' --registry-use k3d-registry.localhost:5500
+
+# Deploy
+helm install broker --atomic oci://mqbuilds.azurecr.io/helm/aio-broker --version 0.7.0-nightly
 
 # Deploy Operator helm chart
 
-# Deploy HTTP server (as an asset)
+# Build HTTP server docker image
+docker build -t http-server:latest ./SampleHttpServer
+docker tag http-server:latest http-server:latest
+#docker push k3d-registry.localhost:5500/http-server:latest
+k3d image import http-server:latest -c k3s-default
 
+# Deploy HTTP server (as an asset)
+kubectl apply -f ./SampleHttpServer/http-server.yaml
