2cc06743977402c70b9a0f7d79cce67e6c92a73b
Dominic Betts <1454644+dominicbetts@users.noreply.github.com>
2024-10-29 12:16:39 +0000

    Update devcontainer.json to enable az login

---
+++ .devcontainer/postStartCommand.sh
@@ -0,0 +1,4 @@
+echo 'alias az-login="CODESPACES=false az login"' >> ~/.bashrc
+source ~/.bashrc
+
+echo -e "Environment: \nSUBSCRIPTION_ID: $SUBSCRIPTION_ID \nRESOURCE_GROUP: $RESOURCE_GROUP \nLOCATION: $LOCATION \nCLUSTER_NAME: $CLUSTER_NAME"
