52f89f5309704729ec251ebdca3c6a7093f8bcab
Dominic Betts <1454644+dominicbetts@users.noreply.github.com>
2024-12-20 17:04:01 +0000

    Media connector updates (#112)
    Updates to media connector sample including those from #111
    
    ---------
    
    Co-authored-by: Sam Chang <samecchang@users.noreply.github.com>

---
+++ samples/media-connector-invoke-test/media-server/Install-MediaServer.ps1
@@ -0,0 +1,28 @@
+﻿<#
+.SYNOPSIS
+	Deploy MediaMTX into the default Kubernetes cluster.
+.DESCRIPTION
+	This PowerShell script deploys MediaMTX into the default Kubernetes cluster using kubectl.
+.EXAMPLE
+	PS> ./media-server-deploy.ps1
+	namespace/media-server created
+	deployment.apps/media-server created
+	service/media-server created
+	service/media-server-public created
+.LINK
+    https://azure.com/
+.NOTES
+    (c) 2024 Microsoft Corporation. All rights reserved.
+	Author: andrejm@microsoft.com
+#>
+
+try {
+    & kubectl create namespace media-server --dry-run=client -o yaml | kubectl apply -f -
+    & kubectl apply -f media-server-deployment.yaml --validate=false
+    & kubectl apply -f media-server-service.yaml --validate=false
+    & kubectl apply -f media-server-service-public.yaml --validate=false
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
