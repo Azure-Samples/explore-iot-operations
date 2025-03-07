b11625fc6314a0ee4d61ab7bbcc9e9315c139dab
davidE <116028030+david-emakenemi@users.noreply.github.com>
2025-03-06 18:13:23 -0800

    add codespace suffix to quickstart clusters
    Adding codespace identifier
    
    ## Purpose
    <!-- Describe the intention of the changes being proposed. What problem
    does it solve or functionality does it add? -->
    * ...
    
    ## Does this introduce a breaking change?
    <!-- Mark one with an "x". -->
    ```
    [ ] Yes
    [- ] No
    ```
    
    ## Pull Request Type
    What kind of change does this Pull Request introduce?
    
    <!-- Please check the one that applies to this PR using "x". -->
    ```
    [- ] Bugfix
    [ ] Feature
    [ ] Code style update (formatting, local variables)
    [ ] Refactoring (no functional changes, no api changes)
    [ ] Documentation content changes
    [ ] Other... Please describe:
    ```
    
    ## How to Test
    *  Get the code
    
    ```
    git clone [repo-address]
    cd [repo-name]
    git checkout [branch-name]
    npm install
    ```
    
    * Test the code
    <!-- Add steps to run the tests suite and/or manually test -->
    ```
    ```
    
    ## What to Check
    Verify that the following are valid
    * ...
    
    ## Other Information
    <!-- Add any other helpful information that may be needed here. -->
    This is related to this bug - [Telemetry can't distinguish between K3s
    on Codespaces (getting started msft infra) vs. K3 on customer
    infra](https://msazure.visualstudio.com/One/_workitems/edit/31306566)
    
    ---------
    
    Co-authored-by: John Lian <jlian@users.noreply.github.com>

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,5 +1,5 @@
 echo 'export CODESPACES="FALSE"' >> ~/.bashrc
-echo 'export CLUSTER_NAME=${CODESPACE_NAME%-*}' >> ~/.bashrc
+echo 'export CLUSTER_NAME=${CODESPACE_NAME%-*}-codespace' >> ~/.bashrc
 source ~/.bashrc
 
 echo -e "Environment: \nSUBSCRIPTION_ID: $SUBSCRIPTION_ID \nRESOURCE_GROUP: $RESOURCE_GROUP \nLOCATION: $LOCATION \nCLUSTER_NAME: $CLUSTER_NAME"
