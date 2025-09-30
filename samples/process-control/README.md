b7b8ae3c22a7ecfa2cebd45e960abacfb788bbc1
Dominic Betts <1454644+dominicbetts@users.noreply.github.com>
2025-09-30 14:56:44 +0100

    Add process control sample (#148)
    ## Purpose
    Add process control sample
    
    ## Does this introduce a breaking change?
    <!-- Mark one with an "x". -->
    ```
    [ ] Yes
    [x] No
    ```
    
    ## Pull Request Type
    What kind of change does this Pull Request introduce?
    
    <!-- Please check the one that applies to this PR using "x". -->
    ```
    [ ] Bugfix
    [ ] Feature
    [ ] Code style update (formatting, local variables)
    [ ] Refactoring (no functional changes, no api changes)
    [x] Documentation content changes
    [ ] Other... Please describe:
    ```

---
+++ samples/process-control/README.md
@@ -0,0 +1,99 @@
+# OPC UA process control
+
+In Azure IoT Operations `aio-opc-ua-commander` lets you send changes to an OPC UA server from the edge or from the cloud. The current preview includes support for writing data points from an asset dataset with simple and complex data-types as well as dumping the address space of an OPC UA server.
+
+The OPC-UA commander:
+
+- Uses the [RPC](https://github.com/Azure/iot-operations-sdks/blob/main/doc/reference/rpc-protocol.md) and MQTT protocol/broker as the underlying messaging plane.
+  MQTT messages include some system and user properties to define [metadata](https://github.com/Azure/iot-operations-sdks/blob/main/doc/reference/message-metadata.md) values that help with flow control.
+- Subscribes to MQTT topic `{AioNamespace}/asset-operations/{AssetId}/{DatasetName}/` for data-set write operations.
+- Subscribes to MQTT topic `{AioNamespace}/asset-operations/{AssetId}/{ManagementGroupName}/` for call operations and explicit write.
+- Subscribes to MQTT topic `{AioNamespace}/endpoint-operations/{InboundEndpointProfileName}/{ActionName}/` for endpoint operations.
+- On MQTT request/response create ad-hoc session based on the device associated with the namespace asset.
+- Validates write requests against the generated request schema.
+- Validates that the write request only contains data points that exist within the dataset.
+- Use write service calls to set all data-points at once.
+- Sends response to response topic property defined in MQTT message.
+
+To learn more about how `aio-opc-ua-commander` works, see [How to control OPC UA assets](https://learn.microsoft.com/azure/iot-operations/discover-manage-assets/howto-control-opc-ua).
+
+This sample illustrates some of these capabilities using the OPC PLC simulator boiler.
+
+## Prerequisites
+
+To run the sample application, you need:
+
+- A preview instance of Azure IoT Operations deployed. If you don't already have an instance, see [Create an Azure IoT Operations instance](https://learn.microsoft.com/azure/iot-operations/get-started-end-to-end-sample/quickstart-deploy).
+- Access to the internal MQTT broker in the Azure IoT Operations cluster. To configure access the broker, see [Test connectivity to MQTT broker with MQTT clients](https://learn.microsoft.com/azure/iot-operations/manage-mqtt-broker/howto-test-connection).
+- [Azure CLI](https://learn.microsoft.com/en-us/cli/azure/install-azure-cli) version 2.67.0 or higher.
+- [NET 9 SDK](https://dotnet.microsoft.com/en-us/download/dotnet/9.0)
+
+## Deploy the simulator
+
+The sample application uses the boiler in the OPC PLC simulator.
+
+To deploy the OPC PLC simulator, run the following command:
+
+```bash
+kubectl apply -f https://raw.githubusercontent.com/Azure-Samples/explore-iot-operations/main/samples/quickstarts/opc-plc-deployment.yaml
+```
+
+> [!CAUTION]
+> This configuration uses a self-signed application instance certificate. Don't use this configuration in a production environment. To learn more, see [Configure OPC UA certificates infrastructure for the connector for OPC UA](https://learn.microsoft.com/azure/iot-operations/discover-manage-assets/howto-configure-opc-ua-certificates-infrastructure).
+
+
+## Configure the device and namespace assets
+
+To add the required device (`opc-ua-commander`), inbound endpoint (`opc-ua-commander-0`), and namespace asset (`boiler`) to your instance, run the following commands:
+
+```bash
+wget https://raw.githubusercontent.com/Azure-Samples/explore-iot-operations/main/samples/process-control/boiler-simulation.bicep -O boiler-simulation.bicep
+
+AIO_NAMESPACE_NAME=<YOUR_AIO_NAMESPACE_NAME>
+RESOURCE_GROUP=<YOUR_RESOURCE_GROUP_NAME>
+SUBSCRIPTION_ID=$(az account show --query id -o tsv)
+CUSTOM_LOCATION_NAME=$(az iot ops list -g $RESOURCE_GROUP --query "[0].extendedLocation.name" -o tsv | awk -F'/' '{print $NF}')
+
+az deployment group create --subscription $SUBSCRIPTION_ID --resource-group $RESOURCE_GROUP --template-file boiler-simulation.bicep --parameters customLocationName=$CUSTOM_LOCATION_NAME aioNamespaceName=$AIO_NAMESPACE_NAME
+```
+
+You can review these resources in the Operations experience web UI or by running the following commands:
+
+```bash
+# View device details
+az iot ops ns device query --instance <your instance name> -g <your resource group>
+# View namespace asset details
+az iot ops ns asset query --instance <your instance name> -g <your resource group>
+```
+
+## Configure and run application
+
+Make sure the `process-control-demo/src/write.dataset.client/appsettings.json` file contains the correct values for:
+
+- `HostName` and `Port` in the `ConnectionStrings` setting.
+- `Namespace`: typically `azure-iot-operations` if you used the quickstarts deployment.
+- `AssetName`: `boiler` if you used the `boiler-simulation.bicep` file to add the device and namespace asset.
+- `DatasetName`: `boiler-simple-write` if you used the `boiler-simulation.bicep` file to add the device and namespace asset.
+
+To run the application:
+
+```bash
+dotnet run --project process-control-demo/src/write.dataset.client/write.dataset.client.csproj
+```
+
+### Usage
+
+For example, to set the `TargetTemperature` and other values on the boiler asset, the sample application publishes the following MQTT message to the topic `azure-iot-operations/asset-operations/<asset name>/<dataset name>`:
+
+```json
+{
+    "BaseTemperature": 42,
+    "MaintenanceInterval": 360,
+    "OverheatInterval": 45,
+    "OverheatedThresholdTemperature": 199,
+    "TargetTemperature": 176,
+    "TemperatureChangeSpeed": 6
+}
+```
+
+The OPC UA commander service in the cluster subscribes to this topic, receives the message, and writes the values to the OPC UA server. The commander service then publishes the result of the write operation to the topic `responseTopic`. If the operation succeeds, the message in the response topic looks like `{}`.
---
+++ samples/process-control/process-control-demo/src/write.dataset.client/Program.cs
@@ -0,0 +1,200 @@
+﻿namespace Aio.Connectors.OpcUa.Demo;
+
+using Azure.Iot.Operations.Mqtt.Session;
+using Azure.Iot.Operations.Protocol;
+using Azure.Iot.Operations.Protocol.Connection;
+using Azure.Iot.Operations.Protocol.Models;
+using Azure.Iot.Operations.Protocol.RPC;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using mrpc.client.generated.v3;
+using System;
+using System.Buffers;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Terminal.Gui;
+
+/// <summary>
+/// Entry point.
+/// </summary>
+public class Program
+{
+    /// <summary>
+    /// Main program.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+    public static async Task Main(string[] args)
+    {
+        // setup logging
+        using var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+        var logger = loggerFactory.CreateLogger("ProcessControlDemo");
+
+        // load configuration
+        IConfiguration configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .AddCommandLine(args)
+            .Build();
+
+        using var cts = new CancellationTokenSource();
+        Console.CancelKeyPress += (_, _) => {
+            logger.LogInformation("SIGINT detected - closing application");
+            cts.Cancel();
+        };
+
+        var sessionClientOptions = new MqttSessionClientOptions
+        {
+            EnableMqttLogging = true,
+        };
+
+        var connectionString = configuration.GetConnectionString("Default");
+        if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            logger.LogError("Please provide connection string via appsettings.json, environment variable or command line argument");
+            Environment.FailFast(null);
+        }
+
+        var mqttConnectionSettings = MqttConnectionSettings.FromConnectionString(connectionString);
+        
+        // Read AIO settings from configuration
+        var aioNamespace = configuration["AioSettings:Namespace"];
+        var assetName = configuration["AioSettings:AssetName"];
+        var datasetName = configuration["AioSettings:DatasetName"];
+        
+        if (string.IsNullOrWhiteSpace(aioNamespace) || string.IsNullOrWhiteSpace(assetName) || string.IsNullOrWhiteSpace(datasetName))
+        {
+            logger.LogError("Please provide AioSettings (Namespace, AssetName, DatasetName) via appsettings.json, environment variables or command line arguments");
+            Environment.FailFast(null);
+        }
+        
+        MqttSessionClient? mqttClient = default;
+        ApplicationContext? applicationContext = default;
+        WriteDatasetClient? mrpcWriteDatasetClient = default;
+        try
+        {
+            mqttClient = new MqttSessionClient(new MqttSessionClientOptions { EnableMqttLogging = true });
+            var connectionResult = await mqttClient.ConnectAsync(mqttConnectionSettings, cts.Token).ConfigureAwait(true);
+            if (connectionResult.ResultCode != MqttClientConnectResultCode.Success)
+            {
+                logger.LogError($"Error while connecting to MQTT Broker {connectionResult.ReasonString} {connectionResult.ResultCode}");
+                Environment.FailFast(null);
+            }
+
+            logger.LogInformation("Successful connected to MQTT Broker");
+
+            applicationContext = new ApplicationContext();
+            mrpcWriteDatasetClient = new WriteDatasetClient(
+                applicationContext,
+                mqttClient,
+                aioNamespace,
+                assetName,
+                datasetName);
+
+            Application.Init();
+            var top = Application.Top;
+            var win = new Window("Process Control Demo")
+            {
+                X = 0,
+                Y = 1,
+                Width = Dim.Fill(),
+                Height = Dim.Fill() - 1,
+            };
+            top.Add(win);
+
+            var baseTempLabel = new Label("Base Temperature:") { X = 3, Y = 2 };
+            var baseTempText = new TextField("42") { X = 40, Y = 2, Width = 40 };
+            baseTempText.Enter += (_) => baseTempText.CursorPosition = baseTempText.Text.Length;
+            var targetTempLabel = new Label("Target Temperature:") { X = 3, Y = 4 };
+            var targetTempText = new TextField("176") { X = 40, Y = 4, Width = 40 };
+            targetTempText.Enter += (_) => targetTempText.CursorPosition = targetTempText.Text.Length;
+            var tempChangeSpeedLabel = new Label("Temperature Change Speed:") { X = 3, Y = 6 };
+            var tempChangeSpeedText = new TextField("6") { X = 40, Y = 6, Width = 40 };
+            tempChangeSpeedText.Enter += (_) => tempChangeSpeedText.CursorPosition = tempChangeSpeedText.Text.Length;
+            var overheatedThresholdTempLabel = new Label("Overheated Threshold Temperature:") { X = 3, Y = 8 };
+            var overheatedThresholdTempText = new TextField("199") { X = 40, Y = 8, Width = 40 };
+            overheatedThresholdTempText.Enter += (_) => overheatedThresholdTempText.CursorPosition = overheatedThresholdTempText.Text.Length;
+            var maintenanceIntervalLabel = new Label("Maintenance Interval:") { X = 3, Y = 10 };
+            var maintenanceIntervalText = new TextField("360") { X = 40, Y = 10, Width = 40 };
+            maintenanceIntervalText.Enter += (_) => maintenanceIntervalText.CursorPosition = maintenanceIntervalText.Text.Length;
+            var overheatIntervalLabel = new Label("Overheat Interval:") { X = 3, Y = 12 };
+            var overheatIntervalText = new TextField("45") { X = 40, Y = 12, Width = 40 };
+            overheatIntervalText.Enter += (_) => overheatIntervalText.CursorPosition = overheatIntervalText.Text.Length;
+
+            var sendButton = new Button("Send") { X = 3, Y = 14 };
+            var closeButton = new Button("Close") { X = 25, Y = 14 };
+
+            sendButton.Clicked += async () =>
+            {
+                var request = $@"
+                {{
+                    ""BaseTemperature"" : {baseTempText.Text},
+                    ""TargetTemperature"" : {targetTempText.Text},
+                    ""TemperatureChangeSpeed"" : {tempChangeSpeedText.Text},
+                    ""OverheatedThresholdTemperature"" : {overheatedThresholdTempText.Text},
+                    ""MaintenanceInterval"" : {maintenanceIntervalText.Text},
+                    ""OverheatInterval"": {overheatIntervalText.Text}
+                }}";
+
+                ReadOnlySequence<byte> buffer = new ReadOnlySequence<byte>(Encoding.UTF8.GetBytes(request));
+                var reqPayload = new CustomPayload(buffer, "application/json", MqttPayloadFormatIndicator.CharacterData);
+
+                try
+                {
+                    var commandRequestMetadata = new CommandRequestMetadata();
+                    commandRequestMetadata.UserData.Add("mqtt-property", "value");
+
+                    mrpcWriteDatasetClient.WriteDatasetCommandInvoker.ResponseTopicPattern = "responseTopic/" + Guid.NewGuid().ToString();
+                    var rpcCall = await mrpcWriteDatasetClient.WriteDatasetAsync(
+                        request: reqPayload,
+                        requestMetadata: commandRequestMetadata,
+                        commandTimeout: TimeSpan.FromMinutes(1),
+                        cancellationToken: cts.Token)
+                        .WithMetadata();
+                    MessageBox.Query("Success", "Request sent successfully!", "Ok");
+                    var responseMetadata = rpcCall.ResponseMetadata;
+                }
+                catch (Exception e)
+                {
+                    MessageBox.ErrorQuery("Error", $"Failed to send request: {e.Message}", "Ok");
+                }
+            };
+
+            closeButton.Clicked += () =>
+            {
+                cts.Cancel();
+                Application.Shutdown();
+            };
+
+            win.Add(
+                baseTempLabel,
+                baseTempText,
+                targetTempLabel,
+                targetTempText,
+                tempChangeSpeedLabel,
+                tempChangeSpeedText,
+                overheatedThresholdTempLabel,
+                overheatedThresholdTempText,
+                maintenanceIntervalLabel,
+                maintenanceIntervalText,
+                overheatIntervalLabel,
+                overheatIntervalText,
+                sendButton,
+                closeButton);
+
+            Application.Run();
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e.ToString());
+        }
+        finally
+        {
+            await (mrpcWriteDatasetClient?.DisposeAsync() ?? ValueTask.CompletedTask).ConfigureAwait(false);
+            await (applicationContext?.DisposeAsync() ?? ValueTask.CompletedTask).ConfigureAwait(false);
+            await (mqttClient?.DisposeAsync() ?? ValueTask.CompletedTask).ConfigureAwait(false);
+        }
+
+        logger.LogInformation("OPC UA Process Control Demo - Stopped!");
+    }
+}
