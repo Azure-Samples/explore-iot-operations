b37f19b913dad13d1261226bf33ad36d1c3d6b7c
Dominic Betts <1454644+dominicbetts@users.noreply.github.com>
2026-01-26 11:44:56 +0000

    Remove old samples

--- lib/proto/gen.sh
+++
@@ -1,3 +0,0 @@
-protoc --go_out=. --go_opt=paths=source_relative \
-    --go-grpc_out=require_unimplemented_servers=false:. --go-grpc_opt=paths=source_relative \
-    message.proto
--- samples/http-sql-connector-app-dotnet/HttpSqlConnector/AuthType.cs
+++
@@ -1,9 +0,0 @@
-namespace ContextualDataIngestor
-{
-    public enum AuthType
-    {
-        Sqlbasic,
-        Httpbasic
-        // Add other types as needed
-    }
-}
